0e46d8c6157cc1e90c588bfd0f6ece3efea982a8
tarafdarmansour <tarafdar.mansour@gmail.com>
2022-06-14 21:32:21 +0400

    Adding middleware

---
+++ IPManager/Middleware/IPCheckMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using IPManager.Services;
+
+namespace IPManager.Middleware;
+
+public class IpCheckMiddleware
+{
+    private readonly IIPCheckService _blockingService;
+    private readonly RequestDelegate _next;
+
+    public IpCheckMiddleware(RequestDelegate next, IIPCheckService blockingService)
+    {
+        _next = next;
+        _blockingService = blockingService;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var remoteIp = context.Connection.RemoteIpAddress;
+        var isBlocked = _blockingService.IsBlocked(remoteIp!);
+        if (isBlocked)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            return;
+        }
+
+        await _next.Invoke(context);
+    }
+}
--- IPManager/Program.cs
+++ IPManager/Program.cs
@@ -1,4 +1,6 @@
+using IPManager.Middleware;
 using IPManager.Services;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +14,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IIPCheckService, IPCheckService>();
 
 var app = builder.Build();
-
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+app.UseMiddleware<IpCheckMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
