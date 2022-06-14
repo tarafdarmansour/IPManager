384e1980e3a3abee955979440be8ea2c1e5ac650
tarafdarmansour <tarafdar.mansour@gmail.com>
2022-06-14 21:29:55 +0400

    Adding IPChecking service and interface

---
+++ IPManager/Services/IIPCheckService.cs
@@ -0,0 +1,8 @@
+﻿using System.Net;
+
+namespace IPManager.Services;
+
+public interface IIPCheckService
+{
+    bool IsBlocked(IPAddress ipAddress);
+}
---
+++ IPManager/Services/IPCheckService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+namespace IPManager.Services;
+
+public class IPCheckService : IIPCheckService
+{
+    private readonly List<string> _blackIps;
+    private readonly List<string> _whiteIps;
+    private readonly bool checkBlackList;
+    private readonly bool checkwhiteList;
+
+    public IPCheckService(IConfiguration configuration)
+    {
+        checkBlackList = configuration.GetValue<bool>("BlackListCheck");
+        checkwhiteList = configuration.GetValue<bool>("WhiteListCheck");
+
+        if (checkwhiteList)
+        {
+            var whiteList = configuration.GetValue<string>("WhiteList");
+            _whiteIps = whiteList.Split(',').ToList();
+        }
+
+        if (checkBlackList)
+        {
+            var blackList = configuration.GetValue<string>("BlackList");
+            _blackIps = blackList.Split(',').ToList();
+        }
+    }
+
+    public bool IsBlocked(IPAddress ipAddress)
+    {
+        if (checkBlackList && _blackIps.Contains(ipAddress.ToString()))
+            return true;
+
+        if (checkwhiteList && !_whiteIps.Contains(ipAddress.ToString()))
+            return true;
+
+        return false;
+    }
+}
